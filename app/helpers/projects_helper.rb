2079c6661cdc3cacd5530e47ec6e1aec3ff2f7e0
Josh Brody <git@josh.mn>
2021-08-31 15:45:12 -0500

    cache individual projects

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,4 +3,19 @@ module ProjectsHelper
     max_updated_at = (projects.except(:group, :order).maximum(:updated_at) || Date.today).to_s(:number)
     "projects/#{projects.map(&:id).join('-')}-#{controller_name}-#{max_updated_at}"
   end
+
+  def cache_key_for_project(project = @project)
+    cache_key_elements = common_project_cache_keys
+    cache_key_elements += [
+      project.cache_key_with_version,
+    ]
+
+    cache_key_elements.compact.join('/')
+  end
+
+  # different meta has different caches, unfortunately.
+  # still speeds up and reduces allocations
+  def common_project_cache_keys
+    [controller_name]
+  end
 end
