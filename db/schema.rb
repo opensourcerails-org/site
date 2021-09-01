6c789468a31926bf3d5896d3f611d045eb3774bb
Josh Brody <git@josh.mn>
2021-09-01 11:44:08 -0500

    schema is annoying

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,186 +10,185 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_831_155_423) do
+ActiveRecord::Schema.define(version: 2021_08_31_155423) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'action_text_rich_texts', force: :cascade do |t|
-    t.string 'name', null: false
-    t.text 'body'
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[record_type record_id name], name: 'index_action_text_rich_texts_uniqueness', unique: true
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
-  create_table 'active_admin_comments', force: :cascade do |t|
-    t.string 'namespace'
-    t.text 'body'
-    t.string 'resource_type'
-    t.bigint 'resource_id'
-    t.string 'author_type'
-    t.bigint 'author_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[author_type author_id], name: 'index_active_admin_comments_on_author'
-    t.index ['namespace'], name: 'index_active_admin_comments_on_namespace'
-    t.index %w[resource_type resource_id], name: 'index_active_admin_comments_on_resource'
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'admin_users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_admin_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_admin_users_on_reset_password_token', unique: true
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table 'ahoy_events', force: :cascade do |t|
-    t.bigint 'visit_id'
-    t.bigint 'user_id'
-    t.string 'name'
-    t.jsonb 'properties'
-    t.datetime 'time'
-    t.index %w[name time], name: 'index_ahoy_events_on_name_and_time'
-    t.index ['properties'], name: 'index_ahoy_events_on_properties', opclass: :jsonb_path_ops, using: :gin
-    t.index ['user_id'], name: 'index_ahoy_events_on_user_id'
-    t.index ['visit_id'], name: 'index_ahoy_events_on_visit_id'
+  create_table "ahoy_events", force: :cascade do |t|
+    t.bigint "visit_id"
+    t.bigint "user_id"
+    t.string "name"
+    t.jsonb "properties"
+    t.datetime "time"
+    t.index ["name", "time"], name: "index_ahoy_events_on_name_and_time"
+    t.index ["properties"], name: "index_ahoy_events_on_properties", opclass: :jsonb_path_ops, using: :gin
+    t.index ["user_id"], name: "index_ahoy_events_on_user_id"
+    t.index ["visit_id"], name: "index_ahoy_events_on_visit_id"
   end
 
-  create_table 'ahoy_visits', force: :cascade do |t|
-    t.string 'visit_token'
-    t.string 'visitor_token'
-    t.bigint 'user_id'
-    t.string 'ip'
-    t.text 'user_agent'
-    t.text 'referrer'
-    t.string 'referring_domain'
-    t.text 'landing_page'
-    t.string 'browser'
-    t.string 'os'
-    t.string 'device_type'
-    t.string 'country'
-    t.string 'region'
-    t.string 'city'
-    t.float 'latitude'
-    t.float 'longitude'
-    t.string 'utm_source'
-    t.string 'utm_medium'
-    t.string 'utm_term'
-    t.string 'utm_content'
-    t.string 'utm_campaign'
-    t.string 'app_version'
-    t.string 'os_version'
-    t.string 'platform'
-    t.datetime 'started_at'
-    t.index ['user_id'], name: 'index_ahoy_visits_on_user_id'
-    t.index ['visit_token'], name: 'index_ahoy_visits_on_visit_token', unique: true
+  create_table "ahoy_visits", force: :cascade do |t|
+    t.string "visit_token"
+    t.string "visitor_token"
+    t.bigint "user_id"
+    t.string "ip"
+    t.text "user_agent"
+    t.text "referrer"
+    t.string "referring_domain"
+    t.text "landing_page"
+    t.string "browser"
+    t.string "os"
+    t.string "device_type"
+    t.string "country"
+    t.string "region"
+    t.string "city"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "utm_source"
+    t.string "utm_medium"
+    t.string "utm_term"
+    t.string "utm_content"
+    t.string "utm_campaign"
+    t.string "app_version"
+    t.string "os_version"
+    t.string "platform"
+    t.datetime "started_at"
+    t.index ["user_id"], name: "index_ahoy_visits_on_user_id"
+    t.index ["visit_token"], name: "index_ahoy_visits_on_visit_token", unique: true
   end
 
-  create_table 'projects', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'slug', null: false
-    t.integer 'rails_major_version', null: false
-    t.string 'github', null: false
-    t.text 'github_about'
-    t.string 'website'
-    t.integer 'contributors', default: 1, null: false
-    t.integer 'dependents'
-    t.integer 'stars', default: 0, null: false
-    t.integer 'watchers', default: 1, null: false
-    t.integer 'forks', default: 0, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'branch'
-    t.datetime 'meta_last_updated_at'
-    t.string 'description'
-    t.text 'short_blurb'
-    t.datetime 'last_activity_at'
-    t.string 'color'
-    t.string 'last_commit'
-    t.datetime 'hidden_at'
-    t.json 'data', default: {}, null: false
-    t.integer 'pulse'
-    t.index ['hidden_at'], name: 'index_projects_on_hidden_at'
-    t.index ['slug'], name: 'index_projects_on_slug', unique: true
+  create_table "projects", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "slug", null: false
+    t.integer "rails_major_version", null: false
+    t.string "github", null: false
+    t.text "github_about"
+    t.string "website"
+    t.integer "contributors", default: 1, null: false
+    t.integer "dependents"
+    t.integer "stars", default: 0, null: false
+    t.integer "watchers", default: 1, null: false
+    t.integer "forks", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "branch"
+    t.datetime "meta_last_updated_at"
+    t.string "description"
+    t.text "short_blurb"
+    t.datetime "last_activity_at"
+    t.string "color"
+    t.string "last_commit"
+    t.datetime "hidden_at"
+    t.json "data", default: {}, null: false
+    t.integer "pulse"
+    t.index ["hidden_at"], name: "index_projects_on_hidden_at"
+    t.index ["slug"], name: "index_projects_on_slug", unique: true
   end
 
-  create_table 'taggings', id: :serial, force: :cascade do |t|
-    t.integer 'tag_id'
-    t.string 'taggable_type'
-    t.integer 'taggable_id'
-    t.string 'tagger_type'
-    t.integer 'tagger_id'
-    t.string 'context', limit: 128
-    t.datetime 'created_at'
-    t.index ['context'], name: 'index_taggings_on_context'
-    t.index %w[tag_id taggable_id taggable_type context tagger_id tagger_type], name: 'taggings_idx',
-                                                                                unique: true
-    t.index ['tag_id'], name: 'index_taggings_on_tag_id'
-    t.index %w[taggable_id taggable_type context], name: 'taggings_taggable_context_idx'
-    t.index %w[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
-    t.index ['taggable_id'], name: 'index_taggings_on_taggable_id'
-    t.index ['taggable_type'], name: 'index_taggings_on_taggable_type'
-    t.index %w[tagger_id tagger_type], name: 'index_taggings_on_tagger_id_and_tagger_type'
-    t.index ['tagger_id'], name: 'index_taggings_on_tagger_id'
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
   end
 
-  create_table 'tags', id: :serial, force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer 'taggings_count', default: 0
-    t.string 'slug', null: false
-    t.jsonb 'data', default: {}, null: false
-    t.integer 'visible_taggings_count', default: 0, null: false
-    t.index ['data'], name: 'index_tags_on_data', using: :gin
-    t.index ['name'], name: 'index_tags_on_name', unique: true
-    t.index ['slug'], name: 'index_tags_on_slug', unique: true
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "taggings_count", default: 0
+    t.string "slug", null: false
+    t.jsonb "data", default: {}, null: false
+    t.integer "visible_taggings_count", default: 0, null: false
+    t.index ["data"], name: "index_tags_on_data", using: :gin
+    t.index ["name"], name: "index_tags_on_name", unique: true
+    t.index ["slug"], name: "index_tags_on_slug", unique: true
   end
 
-  create_table 'updates', force: :cascade do |t|
-    t.datetime 'date'
-    t.string 'title'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "updates", force: :cascade do |t|
+    t.datetime "date"
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'taggings', 'tags'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "taggings", "tags"
 end
