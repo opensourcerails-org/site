56829c8b393ca33925ed4c23e61e6cb7dfbd036b
Josh Brody <2028176+joshmn@users.noreply.github.com>
2022-12-10 12:28:29 -0600

    use right key dumbdumb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,8 +4,6 @@ if Rails.application.config.active_storage.service == :cloudflare
   Aws.config[:credentials] = Aws::Credentials.new(
     Rails.application.credentials.dig(:aws, :access_key_id),
     Rails.application.credentials.dig(:aws, :secret_access_key),
-
   )
-
 end
 
