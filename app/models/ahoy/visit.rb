b5f24607da045708f871ed7bd08b853806253dca
Josh Brody <2028176+joshmn@users.noreply.github.com>
2022-12-11 13:49:53 -0600

    name

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -5,5 +5,13 @@ module Ahoy
     self.table_name = 'ahoy_visits'
 
     has_many :events, class_name: 'Ahoy::Event'
+
+    def name
+      parts = [ip]
+      if region
+        parts << "(#{region})"
+      end
+      parts.compact.join(" ")
+    end
   end
 end
