8f0039c21bf8a583e955572d5f1ff52c80a4f044
Josh Brody <2028176+joshmn@users.noreply.github.com>
2022-12-11 14:24:19 -0600

    osr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@
 class ApplicationController < ActionController::Base
   layout 'inner'
 
+  if Rails.env.production?
+    before_action do
+      if self.request.host == "opensourcerails.org" || request.host == 'www.opensourcerails.org'
+        redirect_to "https://opensourcerails.org#{request.path}", status: :found, allow_other_host: true
+      end
+    end
+  end
   after_action :track_action, if: -> { track_action? }
   skip_before_action :track_ahoy_visit, if: -> { !track_action? }
 
